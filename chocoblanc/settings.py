27ee41717c97d33fbc70f088b93b18de9c37e951
Paul Kane <kanepa1977@gmail.com>
2016-12-01 16:00:37 +0000

    further update

--- chocoblanc/settings.py
+++ chocoblanc/settings.py
@@ -180,7 +180,7 @@ AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME','')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % ''  #this is ricards test.   the above is my stuff
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 
 
