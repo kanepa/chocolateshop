a170f2abf2cedc78acdfc81d488820bf273225a1
Paul Kane <kanepa1977@gmail.com>
2016-12-02 10:05:58 +0000

    url change

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from .views import register, profile, logout, login
 urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^profile/$', profile, name='profile'),
-    url(r'^logout/$', logout, name='logout'),
-    url(r'^login/$', login, name='login'),
+    url(r'^logout/', logout, name='logout'),
+    url(r'^login/', login, name='login'),
 ]
