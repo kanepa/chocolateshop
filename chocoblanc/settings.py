669ac8f51e7052af6eefe08f5712e9b8b1db4fc6
Paul Kane <kanepa1977@gmail.com>
2016-12-16 15:50:55 +0000

    change image

--- chocoblanc/settings.py
+++ chocoblanc/settings.py
@@ -14,14 +14,21 @@ import os
 import dj_database_url
 from boto.s3.connection import S3Connection
 
-
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-''
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+# SECRET_KEY = ''
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')  # this is for Heroku
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG')
-
-ALLOWED_HOSTS = ['chocolateshop.herokuapp.com']
+# DEBUG = True
+ALLOWED_HOSTS = ['chocolateshop.herokuapp.com']   #''127.0.0.1'
 
 
 SITE_ID = 1
