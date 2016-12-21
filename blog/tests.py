7ead8bd8592061685475d1f46b99e046517f2fc7
Paul Kane <kanepa1977@gmail.com>
2016-12-21 21:34:54 +0000

    blog test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from views import post_detail
+
+# Create your tests here.
+from .models import Post
+
+
+class PostTests(TestCase):
+    def test_str(self):
+        test_title = Post(title='My Prefect Brownie')
+        self.assertEqual(str(test_title),
+                         'My Prefect Brownie')
+
+
+
 
 # Create your tests here.
