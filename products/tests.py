571172caa6c95e423da9f58e8c165bab02e00acb
Paul Kane <kanepa1977@gmail.com>
2016-12-21 21:32:52 +0000

    Products test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.shortcuts import render_to_response
+from .models import Product
+
+class ProductsModel(TestCase):
+
+
+    def test_create_product(self):
+        product = Product()
+        product.name = "Single Hearts"
+        product.description = "Our little handmade hearts are special."
+        product.price = 1
+        product.save()
+
+        found = Product.objects.get(name="Single Hearts", price=1)
+        self.assertEqual(found.description, "Our little handmade hearts are special.")
+from django.test import TestCase
 
 # Create your tests here.
